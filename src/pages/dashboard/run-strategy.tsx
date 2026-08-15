3e6bac1f8d41b8f034fd3a5e1fa008a0a0dd40bb
it254a-ux <it.254a@gmail.com>
2026-08-15 22:47:45 +0300

    Add LiveBalance component to RunStrategy

--- src/pages/dashboard/run-strategy.tsx
+++ src/pages/dashboard/run-strategy.tsx
@@ -1,9 +1,10 @@
 import TradeAnimation from '@/components/trade-animation';
+import LiveBalance from '@/components/live-balance/live-balance';
 
 const RunStrategy = () => (
     <div className='toolbar__section' data-testid='dt_run_strategy'>
+        <LiveBalance />
         <TradeAnimation className='toolbar__animation' />
     </div>
 );
-
 export default RunStrategy;
